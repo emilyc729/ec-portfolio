689b90fbd1b3f6eb45fc190b8279bf288ec7e478
Emily <echeun17@mail.ccsf.edu>
2019-10-21 23:42:14 -0700

    update route

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
           <NavBar />
       </header>
       <Switch>
-        <Route exact path="/" component={About} />
+        <Route exact path="/ec-portfolio" component={About} />
         <Route exact path="/projects" component={Projects} />
         <Route exact path="/contact" component={Contact} />
       </Switch>
