715ea39328ec123aa335fe7c1fdf2b1bf949546e
Emily <echeun17@mail.ccsf.edu>
2019-10-21 23:48:36 -0700

    add missing page

--- src/App.js
+++ src/App.js
@@ -16,6 +16,11 @@ function App() {
         <Route exact path="/ec-portfolio" component={About} />
         <Route exact path="/ec-portfolio/projects" component={Projects} />
         <Route exact path="/ec-portfolio/contact" component={Contact} />
+        <Route render={() => 
+          <section className="container">
+          <h1>Error 404: PageNotFound</h1>
+          </section>
+        } />
       </Switch>
     </div>
   );
