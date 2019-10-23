308ccbd5195212ecef49aca22eec1e4c2db05521
Emily <echeun17@mail.ccsf.edu>
2019-10-22 23:54:36 -0700

    update link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function NavBar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <Link className="navbar-brand" to="">Portfolio</Link>
+            <Link className="navbar-brand" to="/ec-portfolio">Portfolio</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#my-navbar" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
